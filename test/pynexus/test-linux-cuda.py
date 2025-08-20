9bd0bd1408350e33622f3016bb4c49405aa112a2
Matthew Leon <matthew.leon.tech@gmail.com>
2025-08-20 10:11:36 -0700

    Update kernels path in pynexus test
    Signed-off-by: Matthew Leon <matthew.leon.tech@gmail.com>

--- test/pynexus/test-linux-cuda.py
+++ test/pynexus/test-linux-cuda.py
@@ -1,8 +1,7 @@
-import numpy
 
 import sys
 import subprocess
-subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
+#subprocess.check_call([sys.executable, "-m", "pip", "install", "torch"])
 
 import torch
 import nexus
@@ -10,7 +9,6 @@ import nexus
 buf0 = torch.ones(1024)
 buf1 = torch.ones(1024)
 res2 = torch.zeros(1024)
-
 
 rt = nexus.get_runtime("cuda")
 
@@ -22,7 +20,7 @@ nb0 = dev.create_buffer(buf0)
 nb1 = dev.create_buffer(buf1)
 nb2 = dev.create_buffer(res2)
 
-lib = dev.load_library_file("build/cuda_kernels/add_vectors.ptx")
+lib = dev.load_library_file("build.local/cuda_kernels/add_vectors.ptx")
 kern = lib.get_kernel('add_vectors')
 
 sched = dev.create_schedule()
@@ -36,8 +34,9 @@ cmd.finalize(32, 32)
 
 sched.run()
 
-#res = nr2.get()
-
 nb2.copy(res2)
 
-print(res2)
+if all(value == 2 for value in res2):
+    print("success")
+else:
+    print("fail")
